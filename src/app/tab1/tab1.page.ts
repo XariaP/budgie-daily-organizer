31222ed8cd7996a393fea38fa405f956f8f4ca8d
prempehx <xaria.prempeh@mail.utoronto.ca>
2022-07-05 10:45:34 -0400

    Implement Shopping List functions: add, edit, remove, and clear items

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { LanguagesService } from '../services/languages.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +10,7 @@ import { LanguagesService } from '../services/languages.service';
 })
 export class Tab1Page {
 
-  constructor(public language: LanguagesService) {}
+  constructor(public language: LanguagesService, public user: UserService, public alertController: AlertController) {}
 
   getLabel(name){
     return this.language.getLabel(name);
@@ -20,9 +22,24 @@ export class Tab1Page {
     {itemName: "Eggs", icon: "egg", color: "success", category: "Breakfast", selected: false},
     {itemName: "Bagels", icon: "egg", color: "warning", category: "Breakfast", selected: false},
   ]
+  
+  edit: boolean = false;
+  currentItem = null;
 
-  selectItem(item){
-    item.selected = !item.selected;
+  setEdit(){
+    this.edit = !this.edit;
+    if (!this.edit){
+      this.currentItem = null;
+    }
+  }
+
+  selectItem(item, edit){
+    if (edit){ //mark for edit;
+      this.currentItem = item;
+    }
+    else{ //mark as obtained
+      item.selected = !item.selected;
+    }
   }
 
   checkSelected(item){
@@ -30,5 +47,115 @@ export class Tab1Page {
       return item.color;
     else
       return "medium";
+  }
+
+  checkEditItem(item){
+    return (this.currentItem == item);
+  }
+
+  getAvatar(){
+    return this.user.getAvatar();
+  }
+
+  addItem(){
+    // this.presentAddAlert();
+
+    var newItem = {
+      itemName: "New Item",
+      icon: "receipt",
+      color: "secondary",
+      category: "Miscellaneous",
+      selected: false
+    };
+    this.list1.splice(0, 0, newItem);
+
+    this.edit = true; //
+    this.selectItem(newItem, this.edit);
+  }
+
+  removeItem(item){
+    this.presentRemoveAlert(item);
+  }
+
+  async presentRemoveAlert(item) {
+    const alert = await this.alertController.create({
+      header: 'Are you sure you want to delete ' + item.itemName + '?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text:'OK',
+          handler: () => {
+            var tempList = this.list1;
+            for (var i = 0; i < tempList.length; i++){
+                if (tempList[i] == item){
+                  this.list1.splice(i, 1);
+                  break;
+                }
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  checkOut(){
+    var updatedList = [];
+    for (var i = 0; i < this.list1.length; i++){
+      if (!this.list1[i].selected){
+        updatedList.push(this.list1[i]);
+      }
+    }
+    this.list1 = updatedList;
+  }
+
+
+  async presentAddAlert() {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: [
+        {
+          text:'OK',
+          handler: item => {
+            var newItem = {
+              itemName: item.name,
+              icon: item.icon,
+              color: item.color,
+              category: item.category,
+              selected: false
+            };
+            this.list1.push(newItem);
+
+            this.setEdit();
+            this.selectItem(newItem, this.edit);
+          }
+        }
+      ],
+      inputs: [
+        {
+          // type: 'text',
+          name: 'name',
+          placeholder: 'Name',
+        },
+        {
+          name: 'icon',
+          placeholder: 'Icon',
+        },
+        {
+          name: 'color',
+          placeholder: 'Colour',
+        },
+        {
+          name: 'category',
+          placeholder: 'Category'
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
